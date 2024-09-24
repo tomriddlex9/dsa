a1588f7c2cf869328be0cdb7725f6803cf8ffb1a
tomriddlex9 <siddhantsiwach1999@gmail.com>
2024-09-25 02:35:31 +0530

    basic IO manipulation

---
+++ basics/escapeSequence.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // main code goes here
+    // using escape sequence to print various characters
+    // on the output screen
+
+    cout << "Hi,\n";
+    cout << "\"Ghost don\'t die\"\n";
+    cout << "Here we use \t tab spacing" << endl;
+}
